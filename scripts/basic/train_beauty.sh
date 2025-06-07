aa2d9f9eb767b0c5484dd5aee74d84f995574295
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2025-06-07 16:43:30 +0900

    [shell] ADD reverse shell

---
+++ scripts/basic/train_beauty.sh
@@ -0,0 +1,13 @@
+cd "$(dirname "$0")"/..
+
+python run_seq.py \
+  --dataset=Amazon_Beauty \
+  --epochs=100 \
+  --hidden_dropout_prob=0.5 \
+  --attn_dropout_prob=0.5 \
+  --use_rl=1 \
+  --joint=0 \
+  train_batch_size=256 \
+  --lmd=0. \
+  --beta=0.05 \
+  --sim='dot' 
